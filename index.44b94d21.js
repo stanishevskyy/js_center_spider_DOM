a74d75d58b1497d7aadedbb7f01ec48ddbc70bda
Your_Name Your_Surname <your@email>
2024-10-18 14:26:18 +0300

    make build

---
+++ index.44b94d21.js
@@ -0,0 +1,2 @@
+var e,t,c,n,i=document.querySelector(".wall"),l=document.querySelector(".spider");e=i.getBoundingClientRect(),t=l.getBoundingClientRect(),c=(e.height-t.height)/2,n=(e.width-t.width)/2,l.style.top="".concat(c,"px"),l.style.left="".concat(n,"px");
+//# sourceMappingURL=index.44b94d21.js.map
